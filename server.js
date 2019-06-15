0df3c965255557a0512dc89741d2bde94be58eef
tim-garner <timothy.garner602@gmail.com>
2019-06-15 18:59:40 -0400

    got app listening on server

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 console.log("Hola");
 
 var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
+  
