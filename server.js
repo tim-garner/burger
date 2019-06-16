3c0cca3e008eec8e4a46ec854b849e07c41f25a8
tim-garner <timothy.garner602@gmail.com>
2019-06-15 23:08:58 -0400

    localhost now working pseudo coded additional info server.js

--- server.js
+++ server.js
@@ -8,8 +8,23 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// var exphbs = require("express-handlebars");
+
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// var routes = require("./controllers/burgers_controller.js");
+
+// app.use(routes);
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
+    console.log("app is now active at localhost" + PORT);
   });
   
   
