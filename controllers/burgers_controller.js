3463209ec285cd2ec02b50cf5e8d864812aa48f5
tim-garner <timothy.garner602@gmail.com>
2019-06-16 21:10:03 -0400

    started burger_controller code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,17 @@
 var express = require("express");
 
 var router = express.Router();
+
+//need data for DB functions. 
+var burger = require("../models/burger.js")
+
+router.get("/", function(req, res){
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
